f92e3fe4a37396fb6f8b3c33085046ea514870b2
hpl <hpl@moving.(none)>
2012-04-27 15:30:40 +0800

    comments

--- latex/tex-table.pl
+++ latex/tex-table.pl
@@ -1,5 +1,11 @@
 #!/usr/bin/perl -w 
 
+#Convert the space separated 2-D table 
+#to the latex tabular format.
+#The former is obviously more convenient 
+#for data process and analysis under linux.
+#However, Latex recognizes the latter. 
+
 use strict ;
 
 while (<STDIN>){
@@ -7,7 +13,12 @@ while (<STDIN>){
 	chomp($line) ;
 	$line =~ s/^\s+//g ;
 	$line =~ s/\s+/\t/g ;
+
+	#the following line does not work!
+	#So I first convert arbitrary space to tab,
+	#and then split using tab..
 	#my @a_line = split /\s+/, $line ;
+
 	my @a_line = split /\t/, $line ;
 	print $a_line[0] ;
 	for (my $i = 1 ; $i < scalar @a_line ; $i ++ ){
