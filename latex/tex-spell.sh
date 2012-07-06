7bda488e429b6cd39f739ae7c7c504320ee64267
hpl <hpl@moving>
2012-07-06 18:07:01 +0800

    deprecated spelling checking script

--- latex/tex-spell.sh
+++ latex/tex-spell.sh
@@ -4,6 +4,28 @@
 #check latex source file using 'aspell'
 #simply list the mis-spelled words
 
+#=========== 20120706
+echo "This script is deprecated."
+echo "Read the script file for explanation."
+echo "Press Ctrl+C to end"
+echo "Press any key to continue..."
+read tmp
+
+#It is known that the cascade command of detex and aspell 
+#generates many non-meaningful entries. 
+#Many of them are just human names, abbreviations. 
+#A more important problem is they do not provide 
+#straightforward warning in the article. 
+#You need to locate them with search function of your editor.
+#Now I advocate to use VIM's spell checking function:
+#
+#:set spell
+#
+#for more information: 
+#   http://vimdoc.sourceforge.net/htmldoc/spell.html
+
+#===========
+
 if [[ $# == 1 ]] ; then
 	file="$1"
 else 
